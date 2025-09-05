91a6477bd2020d1c161d81af9e28d939c20f0de6
Mystery-glitch <kchansda27@gmail.com>
2025-09-05 09:58:10 +0530

    adding setup.py file

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+HYPEN_EDOT='-e .'
+def getRequirements(filePath):
+    requirements=[]
+    with open(filePath) as file:
+        requirements=file.readlines()
+        requirements=[req.replace('\n', '') for req in requirements]
+        
+        if HYPEN_EDOT in requirements:
+            requirements.remove(HYPEN_EDOT)
+    
+    return requirements
+
+setup(
+    name='MalariaDetection',
+    version='0.0.1',
+    author='Group',
+    packages=find_packages(),
+    install_requires=getRequirements('requirements.txt')
+)
